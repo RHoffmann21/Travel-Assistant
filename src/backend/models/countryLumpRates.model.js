16860c23cd538bf0401ca0ffe7c04baf1de84cf4
Ricardo Hoffmann <ricardo.hoffmann@dts.de>
2024-03-14 16:47:35 +0100

    minor changes to Q&A + updated some models

--- src/backend/models/countryLumpRates.model.js
+++ src/backend/models/countryLumpRates.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const countryLumpRatesSchema = new mongoose.Schema({
-  country: {type: mongoose.Schema.Types.ObjectId, ref: 'Country'},
+  country: {type: mongoose.Schema.Types.ObjectId, ref: 'Country', required: true},
   rates: {
     arrivalDepartureDay: { type: Number, required: true },
     fullDay:  { type: Number, required: true }, 
--- src/backend/models/travelExpenseReport.model.js
+++ src/backend/models/travelExpenseReport.model.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const travelExpenseReportSchema = new mongoose.Schema({
-  year: { type: Number },
-  month: { type: Number },
-  chatBubbles: {type: mongoose.Schema.Types.ObjectId, ref: 'Chat' },
-  trip: [{type: mongoose.Schema.Types.ObjectId, ref: 'Trip' }]
+  year: { type: Number, required: true },
+  month: { type: Number, required: true },
+  chatBubbles: {type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
+  trip: [{type: mongoose.Schema.Types.ObjectId, ref: 'Trip', required: true }]
 });
 
 export default mongoose.model('TravelExpenseReport', travelExpenseReportSchema);
