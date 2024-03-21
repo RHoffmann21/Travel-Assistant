6269891d77c888a0adf3e62dafe891083dde8b4e
Ricardo Hoffmann <ricardo.hoffmann@dts.de>
2024-03-21 16:30:55 +0100

    added function to connect to db

---
+++ src/backend/lib/connectDB.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import logger from 'dts-node-logger';
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+/**
+ * @description This function creates a connection between the application and the mongo database
+ * @param {String} mongoUri The connection uri for the mongo database 
+ */
+export default async function connectDB (mongoUri) {
+  if (!mongoUri) {
+    logger.error('No mongoUri provided.');
+  }
+  try {
+    await mongoose.connect(mongoUri, options);
+    logger.debug('Now connected to: ', mongoUri);
+  } catch (err) {
+    logger.error(`Something went wrong while connecting to uri ${mongoUri}. Error: `, err);
+  }
+
+  const mongo = mongoose.connection;
+  mongo.on('error', error => { logger.error('mongo: ' + error.name); });
+  mongo.on('connected', () => { logger.info('mongo: Connected'); });
+  mongo.on('disconnected', () => { logger.info('mongo: Disconnected'); });
+  mongo.once('open', () => { logger.info('Mongodb Connection Successful'); });
+};
